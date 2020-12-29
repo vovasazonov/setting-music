2a90239c0cda521e256e97a6eaad3bf303e91f06
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 10:03:09 +0300

    fix: refactore tests

--- SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerControllerTest.cs
+++ SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerControllerTest.cs
@@ -12,33 +12,21 @@ namespace Tests
         [TestCase(2,1)]
         public void IsAmountPlayingLessLimit_AudioPlayersPlayingNotMoreLimit_ReturnTrue(int limit, int amountPlaying)
         {
-            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
-            audioPlayerDescription.LimitPlayTogether.Returns(limit);
-            var audioSourcePool = Substitute.For<IAudioSourcePool>();
-            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
+            var audioPlayerController = CreateAuiAudioPlayerController(limit);
 
-            for (int i = 0; i < amountPlaying; i++)
-            {
-                audioPlayerController.GetAudioPlayer();
-            }
-            
+            var audioPlayers = GetAudioPlayers(amountPlaying, audioPlayerController);
+
             Assert.IsTrue(audioPlayerController.IsAmountPlayingLessLimit());
         }
-        
+
         [Test]
         [TestCase(2,3)]
         public void IsAmountPlayingLessLimit_AudioPlayersPlayingMoreLimit_ReturnFalse(int limit, int amountPlaying)
         {
-            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
-            audioPlayerDescription.LimitPlayTogether.Returns(limit);
-            var audioSourcePool = Substitute.For<IAudioSourcePool>();
-            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
+            AudioPlayerController audioPlayerController = CreateAuiAudioPlayerController(limit);
 
-            for (int i = 0; i < amountPlaying; i++)
-            {
-                audioPlayerController.GetAudioPlayer();
-            }
-            
+            var audioPlayers = GetAudioPlayers(amountPlaying, audioPlayerController);
+
             Assert.IsFalse(audioPlayerController.IsAmountPlayingLessLimit());
         }
         
@@ -46,16 +34,10 @@ namespace Tests
         [TestCase(2,2)]
         public void IsAmountPlayingLessLimit_AudioPlayersPlayingEqualsLimit_ReturnFalse(int limit, int amountPlaying)
         {
-            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
-            audioPlayerDescription.LimitPlayTogether.Returns(limit);
-            var audioSourcePool = Substitute.For<IAudioSourcePool>();
-            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
+            AudioPlayerController audioPlayerController = CreateAuiAudioPlayerController(limit);
 
-            for (int i = 0; i < amountPlaying; i++)
-            {
-                audioPlayerController.GetAudioPlayer();
-            }
-            
+            var audioPlayers = GetAudioPlayers(amountPlaying, audioPlayerController);
+
             Assert.IsFalse(audioPlayerController.IsAmountPlayingLessLimit());
         }
         
@@ -63,21 +45,10 @@ namespace Tests
         [TestCase(2,5, 4)]
         public void IsAmountPlayingLessLimit_DisposeSpaceToLessLimit_AmountPlayersLessLimit(int limit, int amountPlaying, int amountDispose)
         {
-            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
-            audioPlayerDescription.LimitPlayTogether.Returns(limit);
-            var audioSourcePool = Substitute.For<IAudioSourcePool>();
-            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
-            var audioPlayers = new List<IAudioPlayer>();
-            
-            for (int i = 0; i < amountPlaying; i++)
-            {
-                audioPlayers.Add(audioPlayerController.GetAudioPlayer());
-            }
+            AudioPlayerController audioPlayerController = CreateAuiAudioPlayerController(limit);
 
-            for (int i = 0; i < amountDispose; i++)
-            {
-                audioPlayers[i].Dispose();
-            }
+            var audioPlayers = GetAudioPlayers(amountPlaying, audioPlayerController);
+            DisposeAudioPlayers(amountDispose, audioPlayers);
             
             Assert.IsTrue(audioPlayerController.IsAmountPlayingLessLimit());
         }
@@ -85,13 +56,40 @@ namespace Tests
         [Test]
         public void GetAudioPlayer_NotNull_True()
         {
-            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
-            var audioSourcePool = Substitute.For<IAudioSourcePool>();
-            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
+            AudioPlayerController audioPlayerController = CreateAuiAudioPlayerController(0);
 
             var audioPlayer = audioPlayerController.GetAudioPlayer();
             
             Assert.IsNotNull(audioPlayer);
+        }
+
+        private List<IAudioPlayer> GetAudioPlayers(int amount, IAudioPlayerController audioPlayerController)
+        {
+            var players = new List<IAudioPlayer>();
+            
+            for (int i = 0; i < amount; i++)
+            {
+                players.Add(audioPlayerController.GetAudioPlayer());
+            }
+
+            return players;
+        }
+        
+        private static void DisposeAudioPlayers(int amountDispose, List<IAudioPlayer> audioPlayers)
+        {
+            for (int i = 0; i < amountDispose; i++)
+            {
+                audioPlayers[i].Dispose();
+            }
+        }
+        
+        private static AudioPlayerController CreateAuiAudioPlayerController(int limit)
+        {
+            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
+            audioPlayerDescription.LimitPlayTogether.Returns(limit);
+            var audioSourcePool = Substitute.For<IAudioSourcePool>();
+            AudioPlayerController audioPlayerController = new AudioPlayerController(audioPlayerDescription, audioSourcePool);
+            return audioPlayerController;
         }
     }
 }

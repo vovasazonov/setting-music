db35337627707f2cabae8ffb431b90db50489929
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 11:49:52 +0300

    add: test stop

--- SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerTest.cs
+++ SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerTest.cs
@@ -7,6 +7,19 @@ namespace Tests
     [TestFixture]
     public class AudioPlayerTest
     {
-        
+        [Test]
+        public void Stop_AudioPlayerDisposing_AudioDisposingTrue()
+        {
+            bool isDisposingCalled = false;
+            var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
+            var audioSource = Substitute.For<IAudioSource>();
+            audioSource.When(a=>a.Stop()).Do(a=>audioSource.Stopped += Raise.Event<StoppedHandler>());
+            var audioPlayer = new AudioPlayer(audioPlayerDescription, audioSource);
+            audioPlayer.Disposing += dependency => isDisposingCalled = true;
+            
+            audioPlayer.Stop();
+            
+            Assert.IsTrue(isDisposingCalled);
+        }
     }
 }

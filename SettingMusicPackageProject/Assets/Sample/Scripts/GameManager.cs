b095a1ef6e3db7474d7b84672b4e5c66f503a063
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 14:54:18 +0300

    fix: rename to more clearable

--- SettingMusicPackageProject/Assets/Sample/Scripts/GameManager.cs
+++ SettingMusicPackageProject/Assets/Sample/Scripts/GameManager.cs
@@ -1,17 +1,18 @@
 ﻿using Audio;
 using Sample.Scripts;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public sealed class GameManager : MonoBehaviour
 {
     [SerializeField] private AudioSourcePool _audioSourcePool;
     [SerializeField] private AudioDatabase _audioDatabase;
     [SerializeField] private AudioClipDatabase _audioClipDatabase;
-    [SerializeField] private ButtonView _zombieButtonView;
-    [SerializeField] private ButtonView _increaseVolumeZombieButtonView;
-    [SerializeField] private ButtonView _decreaseVolumeZombieButtonView;
-    [SerializeField] private ButtonView _muteVolumeZombieButtonView;
-    [SerializeField] private ButtonView _stopCollectionButtonView;
+    [SerializeField] private ButtonView _playAudioButtonView;
+    [SerializeField] private ButtonView _increaseVolumeAudioCollectionButtonView;
+    [SerializeField] private ButtonView _decreaseVolumeAudioCollectionButtonView;
+    [SerializeField] private ButtonView _muteAudioCollectionButtonView;
+    [SerializeField] private ButtonView _stopAudioCollectionButtonView;
 
     private IAudioManager _audioManager;
 
@@ -23,10 +24,10 @@ public sealed class GameManager : MonoBehaviour
         var zombieModel = new ZombieModel(_audioManager, "zombie_audio_player", "general");
         var collectionVolumeModel = new AudioCollectionModel(_audioManager.AudioCollections["general"], 0.1f);
         
-        var zombiePresenter = new CharacterPresenter(_zombieButtonView, zombieModel);
-        var increaseAudioCollectionVolumePresenter = new IncreaseVolumeAudioCollectionPresenter(_increaseVolumeZombieButtonView, collectionVolumeModel);
-        var decreaseAudioCollectionVolumePresenter = new DecreaseVolumeAudioCollectionPresenter(_decreaseVolumeZombieButtonView, collectionVolumeModel);
-        var muteAudioCollectionPresenter = new MuteAudioCollectionPresenter(_muteVolumeZombieButtonView, collectionVolumeModel);
-        var stopAudioCollectionPresenter = new StopAudioCollectionPresenter(_stopCollectionButtonView, collectionVolumeModel);
+        var zombiePresenter = new CharacterPresenter(_playAudioButtonView, zombieModel);
+        var increaseAudioCollectionVolumePresenter = new IncreaseVolumeAudioCollectionPresenter(_increaseVolumeAudioCollectionButtonView, collectionVolumeModel);
+        var decreaseAudioCollectionVolumePresenter = new DecreaseVolumeAudioCollectionPresenter(_decreaseVolumeAudioCollectionButtonView, collectionVolumeModel);
+        var muteAudioCollectionPresenter = new MuteAudioCollectionPresenter(_muteAudioCollectionButtonView, collectionVolumeModel);
+        var stopAudioCollectionPresenter = new StopAudioCollectionPresenter(_stopAudioCollectionButtonView, collectionVolumeModel);
     }
 }

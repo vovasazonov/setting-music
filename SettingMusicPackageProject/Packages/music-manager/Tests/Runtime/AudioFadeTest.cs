64790b7aee7d1f5726eba0f1344a455e1ca5e1ad
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 12:58:34 +0300

    add: AudioFadeTest

--- SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioFadeTest.cs
+++ SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioFadeTest.cs
@@ -1,10 +1,44 @@
 ﻿using NUnit.Framework;
 using Audio;
+using NSubstitute;
 
 namespace Tests
 {
     [TestFixture]
     public class AudioFadeTest
     {
+        [Test]
+        public void StartFadeIn_IsFading_True()
+        {
+            var volume = Substitute.For<IVolume>();
+            var audioFade = new AudioFade(volume);
+            
+            audioFade.StartFadeIn();
+            
+            Assert.IsTrue(audioFade.IsFading);
+        }
+        
+        [Test]
+        public void StartFadeOut_IsFading_True()
+        {
+            var volume = Substitute.For<IVolume>();
+            var audioFade = new AudioFade(volume);
+            
+            audioFade.StartFadeOut();
+            
+            Assert.IsTrue(audioFade.IsFading);
+        }
+        
+        [Test]
+        public void StopFade_IsFading_True()
+        {
+            var volume = Substitute.For<IVolume>();
+            var audioFade = new AudioFade(volume);
+            
+            audioFade.StartFadeIn();
+            audioFade.StopFade();
+
+            Assert.IsFalse(audioFade.IsFading);
+        }
     }
 }

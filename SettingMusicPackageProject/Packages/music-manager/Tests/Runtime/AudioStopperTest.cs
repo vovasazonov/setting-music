650d07bf981145ed017f7ff312e64941437247a1
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 10:14:38 +0300

    add: AudioStopperTest

---
+++ SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioStopperTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Audio;
+
+namespace Tests
+{
+    [TestFixture]
+    public class AudioStopperTest
+    {
+        [Test]
+        public void Stop_AudioPlayerIsNull_NotThrowException()
+        {
+            var audioStopper = new AudioStopper(null);
+
+            Assert.DoesNotThrow(()=>audioStopper.Stop());
+        }
+    }
+}

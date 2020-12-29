d54a9753dc22e21122dd7df3d16dcb9b23ea918d
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-29 12:02:07 +0300

    fix: customize code

--- SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerTest.cs
+++ SettingMusicPackageProject/Packages/music-manager/Tests/Runtime/AudioPlayerTest.cs
@@ -10,11 +10,11 @@ namespace Tests
         [Test]
         public void Stop_AudioPlayerDisposing_AudioDisposingTrue()
         {
-            bool isDisposingCalled = false;
             var audioPlayerDescription = Substitute.For<IAudioPlayerDescription>();
             var audioSource = Substitute.For<IAudioSource>();
-            audioSource.When(a=>a.Stop()).Do(a=>audioSource.Stopped += Raise.Event<StoppedHandler>());
             var audioPlayer = new AudioPlayer(audioPlayerDescription, audioSource);
+            bool isDisposingCalled = false;
+            audioSource.When(a=>a.Stop()).Do(a=>audioSource.Stopped += Raise.Event<StoppedHandler>());
             audioPlayer.Disposing += dependency => isDisposingCalled = true;
             
             audioPlayer.Stop();

56ec914894cc9eb01158de87a3e66e192470e47a
Vladimir <48253536+vovasazonov@users.noreply.github.com>
2020-12-30 19:45:48 +0300

    add: sound follow play button

--- SettingMusicPackageProject/Assets/Sample/Scripts/GameManager.cs
+++ SettingMusicPackageProject/Assets/Sample/Scripts/GameManager.cs
@@ -20,7 +20,8 @@ public sealed class GameManager : MonoBehaviour
         _audioSourcePool.Init(_audioClipDatabase.AudioClipDic);
         _audioManager = new AudioManager(_audioDatabase, _audioSourcePool);
 
-        var zombieModel = new ZombieModel(_audioManager, "zombie_audio_player", "general");
+        var playSetting = new PlaySetting(followTransform: new UnityTransform(_playAudioButtonView.transform));
+        var zombieModel = new ZombieModel(_audioManager, "zombie_audio_player", "general",playSetting);
         var collectionVolumeModel = new AudioCollectionModel(_audioManager.AudioCollections["general"], 0.1f);
         
         var zombiePresenter = new CharacterPresenter(_playAudioButtonView, zombieModel);
--- SettingMusicPackageProject/Assets/Sample/Scripts/ZombieModel.cs
+++ SettingMusicPackageProject/Assets/Sample/Scripts/ZombieModel.cs
@@ -7,17 +7,19 @@ namespace Sample.Scripts
         private readonly IAudioManager _audioManager;
         private readonly string _shoutZombieAudioId;
         private readonly string _audioCollectionId;
+        private readonly PlaySetting _playSetting;
 
-        public ZombieModel(IAudioManager audioManager, string shoutZombieAudioId, string audioCollectionId)
+        public ZombieModel(IAudioManager audioManager, string shoutZombieAudioId, string audioCollectionId, PlaySetting playSetting)
         {
             _audioManager = audioManager;
             _shoutZombieAudioId = shoutZombieAudioId;
             _audioCollectionId = audioCollectionId;
+            _playSetting = playSetting;
         }
 
         public void HitMe()
         {
-            _audioManager.Play(_shoutZombieAudioId, _audioCollectionId);
+            _audioManager.Play(_shoutZombieAudioId, _audioCollectionId, _playSetting);
         }
     }
 }
